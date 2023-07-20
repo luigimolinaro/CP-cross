d7bea2a1b85853afe617a5f052281b388fdee877
root <root@dazzling1.fyre.ibm.com>
2023-07-20 11:18:22 +0200

    modifica typo

--- patch/1.priority-cp4.bash
+++ patch/1.priority-cp4.bash
@@ -1,3 +1,3 @@
-oc patch CatalogSource ibm-operator-catalog -n openshiftmarketplace \
+oc patch CatalogSource ibm-operator-catalog -n openshift-marketplace \
 --type=merge \
 --patch '{"spec":{"priority":-1}}'
